d766360f4062bef5bcab2601ff47b69dd4aceff4
coderfish <18755634050@163.com>
2022-06-19 16:57:19 +0800

    粒子偏移和颜色混合

--- src/script.js
+++ src/script.js
@@ -20,7 +20,11 @@ parameters.count = 10000;
 parameters.size = 0.01;
 parameters.radius = 5; // 半径
 parameters.branches = 3;
-parameters.spin = 1;
+parameters.spin = 1; // 旋转的角度
+parameters.randomness = 0.02; // 随机性
+parameters.randomnessPower = 3; // 幂
+parameters.insideColor = '#ff6030';
+parameters.outsideColor = '#1b3984';
 
 let geometry = null;
 let material = null;
@@ -35,26 +39,47 @@ const generateGalaxy = () => {
 
   geometry = new THREE.BufferGeometry();
   const positions = new Float32Array(parameters.count * 3);
+  const colors = new Float32Array(parameters.count * 3);
+  const colorInside = new THREE.Color(parameters.insideColor);
+  const colorOutside = new THREE.Color(parameters.outsideColor);
   for (let i = 0; i < parameters.count; i++) {
     const i3 = i * 3;
 
     const radius = Math.random() * parameters.radius;
     // i % parameters.branches 以3为例 012 012 012
     // 再 / parameters.branches 0 0.333 0.666
-    // 最后*2Π 计算出每一份需要的度数
+    // 最后*2PI 计算出每一份需要的度数
     const branchAngle = ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
     const spinAngle = radius * parameters.spin; // 制造螺旋状
 
-    positions[i3 + 0] = Math.sin(branchAngle + spinAngle) * radius; // x
-    positions[i3 + 1] = 0; // y
-    positions[i3 + 2] = Math.cos(branchAngle + spinAngle) * radius; // z
+    // 每个向量上的偏移量
+    const randomX =
+      Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1);
+    const randomY =
+      Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1);
+    const randomZ =
+      Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1);
+
+    positions[i3 + 0] = Math.cos(branchAngle + spinAngle) * radius + randomX; // x
+    positions[i3 + 1] = 0 + randomY; // y
+    positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ; // z
+
+    const mixedColor = colorInside.clone(); // 不可以直接混合颜色，需要克隆出来再融合
+    // alpha参数可以被认为是两种颜色之间的比例值， 其中0是当前颜色和1.0是第一个参数的颜色。
+    mixedColor.lerp(colorOutside, radius / parameters.radius);
+    colors[i3] = mixedColor.r;
+    colors[i3 + 1] = mixedColor.g;
+    colors[i3 + 2] = mixedColor.b;
   }
   geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+  geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
   material = new THREE.PointsMaterial({
     size: parameters.size,
     sizeAttenuation: true,
     depthWrite: false,
-    blending: THREE.AdditiveBlending
+    blending: THREE.AdditiveBlending,
+    // color: '#ff5588'
+    vertexColors: true // 设置顶点颜色
   });
   points = new THREE.Points(geometry, material);
   scene.add(points);
@@ -66,6 +91,10 @@ gui.add(parameters, 'size').min(0.01).max(0.1).step(0.01).onFinishChange(generat
 gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy);
 gui.add(parameters, 'branches').min(2).max(10).step(1).onFinishChange(generateGalaxy);
 gui.add(parameters, 'spin').min(-5).max(5).step(0.01).onFinishChange(generateGalaxy);
+gui.add(parameters, 'randomness').min(0).max(2).step(0.01).onFinishChange(generateGalaxy);
+gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.01).onFinishChange(generateGalaxy);
+gui.add(parameters, 'insideColor').onFinishChange(generateGalaxy);
+gui.add(parameters, 'outsideColor').onFinishChange(generateGalaxy);
 /**
  * Sizes
  */
